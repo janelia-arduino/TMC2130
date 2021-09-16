de28b2c0fef809fec7eab8818d287d48ec3272a1
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 14:49:08 -0400

    Update email address

--- src/TMC2130.cpp
+++ src/TMC2130.cpp
@@ -2,7 +2,7 @@
 // TMC2130.cpp
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #include "TMC2130.h"
 
--- src/TMC2130.h
+++ src/TMC2130.h
@@ -2,7 +2,7 @@
 // TMC2130.h
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 
 #ifndef TMC2130_H
